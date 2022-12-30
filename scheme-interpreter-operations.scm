a9a3b7a7a9d5f1d8d41240b3b09de0a7fb0e1ac3
DanielHalachev <dani22h09_pl@abv.bg>
2022-12-30 10:49:10 +0200

    Fix file distibution

--- scheme-interpreter-operations.scm
+++ scheme-interpreter-operations.scm
@@ -1,145 +1,133 @@
-;; this file contains all operations - helper functions
+#lang r5rs
+(#%require "scheme-interpreter-operations.scm")
+(#%provide (all-defined))
 
-; retrieves the value of a variable in a specific environment
-(define (lookup-variable-value var env)
-    (define (env-loop env)
-      (define (scan vars valsс)
-        (cond ((null? vars) (env-loop (enclosing-environment env)))
-              ((eq? var (car vars)) (car vals))
-              (else (scan (cdr vars) (cdr vals)))))
-      (if (eq? env the-empty-environment)
-          (error "Unbound variable" var)
-          (let ((frame (first-frame env)))
-            (scan (frame-variables frame) (frame-values frame)))))
-    (env-loop env))
+;; this part contains all auxilliary functions
 
-; sets the specific variable a specific value in a given environment
-(define (set-variable-value! var val env)
-  (define (env-loop env)
-    (define (scan vars vals)
-      (cond ((null? vars) (env-loop (enclosing-environment env)))
-            ((eq? var (car vars)) (set-car! vals val))
-            (else (scan (cdr vars) (cdr vals)))))
-    (if (eq? env the-empty-environment)
-        (error "Unbound variable -- SET!" var)
-        (let ((frame (first-frame env)))
-          (scan (frame-variables frame) (frame-values frame)))))
-  (env-loop env))
+;; basic types and checks
+; default environment definition
+(define default-environment '())
 
-; creates a new environment, extending the given one
-; each environment has its own list of variables and values
-(define (extend-environment vars vals base-env)
-    (if (= (length vars) (length vals))
-        (make-frame vars vals base-env)
-        (if (< (length vars) (length vals))
-            (error "Too many arguments supplied" vars vals)
-            (error "Too few arguments supplied" vars vals))))
+; special form check
+(define (special-form? expression)
+  (member expression '(cond list and or values define set! quote)))
 
-; evaluates a list of expressions in a given environment
-; each expression is mapped to its value in the environment
-(define (list-of-values exps env)
-    (if (no-operands? exps)
-        '()
-        (cons (eval (first-operand exps) env)
-              (list-of-values (rest-operands exps) env))))
+; built-in procedure check
+(define (built-in-procedure? expression)
+  (member expression '(if cond car list? and or values cdr map quote foldr lambda define set!)))
 
-; applies a function to a list of arguments
-(define (apply proc args)
-  (cond ((primitive-procedure? proc) (apply-primitive-procedure proc args))
-        ((compound-procedure? proc) (eval-sequence (procedure-body proc)
-                                                   (extend-environment (procedure-parameters proc)
-                                                                       args
-                                                                       (procedure-environment proc))))
-        (else (error "Unknown procedure type -- APPLY" proc))))
+;; environment setup and operations
+; get from environment
+(define (get-from-environment name)
+  (cadr (assoc name default-environment)))
 
-; primitive procedures
-(define (apply-addition args)
-    (if (null? args)
-        0
-        (+ (car args) (apply-addition (cdr args)))))
-  
-(define (apply-subtraction args)
-  (if (null? (cdr args))
-      (car args)
-      (- (car args) (apply-subtraction (cdr args)))))
-  
-(define (apply-multiplication args)
-  (if (null? args)
-      1
-      (* (car args) (apply-multiplication (cdr args)))))
-  
-(define (apply-division args)
-  (if (null? (cdr args))
-      (car args)
-      (/ (car args) (apply-division (cdr args)))))
+; set value in environment
+(define (set-in-environment name value)
+  (set-car! (cdr (assoc name default-environment))
+            value))
 
-; compound procedure handlers
-(define (make-procedure parameters body env)
-  (cons 'procedure (cons parameters (cons body (cons env '())))))
+; bind a procedure with its value in the default environment
+(define (setup-procedure name value)
+  (let ((old (assoc name default-environment)))
+    (if old
+        (set-car! (cdr old) value)
+        (set! default-environment
+              (cons (list name value) default-environment)))))
 
-; adds primitive procedures to the environment
-(define (extend-primitive-procedures env)
-    (define (extend-procedure proc)
-      (define (dispatch-lambda-primitive-procedure x)
-        (cond ((eq? x 'null?) (lambda (x) (null? x)))
-              ((eq? x 'boolean?) (lambda (x) (boolean? x)))
-              ((eq? x 'symbol?) (lambda (x) (symbol? x)))
-              ((eq? x 'integer?) (lambda (x) (integer? x)))
-              ((eq? x 'char?) (lambda (x) (char? x)))
-              ((eq? x 'string?) (lambda (x) (string? x)))
-              ((eq? x 'pair?) (lambda (x) (pair? x)))
-              ((eq? x 'procedure?) (lambda (x) (procedure? x)))
-              ((eq? x 'car) (lambda (x) (car x)))
-              ((eq? x 'cdr) (lambda (x) (cdr x)))
-              ((eq? x 'set-car!) (lambda (x y) (set-car! x y)))
-              ((eq? x 'set-cdr!) (lambda (x y) (set-cdr! x y)))
-              ((eq? x 'cons) cons)
-              ((eq? x 'list) list)
-              ((eq? x 'eq?) eq?)
-              ((eq? x 'equal?) equal?)
-              (else (error "Unknown primitive procedure -- DISPATCH-LAMBDA-PRIMITIVE-PROCEDURE"
-                           proc))))
-      (set-car! proc (dispatch-lambda-primitive-procedure proc)))
-    (map extend-procedure '(null? boolean? symbol? integer? char?
-                                  string? pair? procedure? car cdr
-                                  set-car! set-cdr! cons list eq? equal?))
-    env)
+(setup-procedure 'not not)
+(setup-procedure 'even? even?)
+(setup-procedure 'odd? odd?)
+(setup-procedure 'number? number?)
+(setup-procedure 'boolean? boolean?)
+(setup-procedure 'equal? equal?)
+(setup-procedure 'eq? eq?)
 
-; add primitive constructors to the environment
-(define (extend-primitive-constructors env)
-      (define (extend-constructor proc)
-        (define (dispatch-lambda-primitive-constructor x)
-          (cond ((eq? x 'make-symbol) (lambda (x) (string->symbol x)))
-                ((eq? x 'make-string) (lambda (x) (make-string x)))
-                ((eq? x 'make-vector) (lambda (x) (make-vector x)))
-                ((eq? x 'symbol->string) (lambda (x) (symbol->string x)))
-                ((eq? x 'string->symbol) (lambda (x) (string->symbol x)))
-                (else (error "Unknown primitive constructor -- DISPATCH-LAMBDA-PRIMITIVE-CONSTRUCTOR"
-                             proc))))
-        (set-car! proc (dispatch-lambda-primitive-constructor proc)))
-      (map extend-constructor '(make-symbol make-string make-vector
-                                symbol->string string->symbol))
-      env)
+(setup-procedure 'null? null?)
+(setup-procedure 'list? list?)
+(setup-procedure 'car car)
+(setup-procedure 'cdr cdr)
+(setup-procedure 'append append)
+(setup-procedure 'cons cons)
 
-; adds primitive I/O operations to the environment
-(define (extend-primitive-I/O-procedures env)
-        (define (extend-procedure proc)
-          (define (dispatch-lambda-primitive-I/O-procedure x)
-            (cond ((eq? x 'display) (lambda (x) (display x)))
-              ((eq? x 'newline) (lambda () (newline)))
-              ((eq? x 'read) (lambda () (read)))
-              ((eq? x 'read-char) (lambda () (read-char)))
-              ((eq? x 'peek-char) (lambda () (peek-char)))
-              ((eq? x 'char-ready?) (lambda () (char-ready?)))
-              (else (error "Unknown primitive I/O procedure -- DISPATCH-LAMBDA-PRIMITIVE-I/O-PROCEDURE"
-                           proc))))
-          (set-car! proc (dispatch-lambda-primitive-I/O-procedure proc)))
-        (map extend-procedure '(display newline read read-char peek-char char-ready?))
-    env)
+(setup-procedure 'quotient quotient)
+(setup-procedure 'remainder remainder)
 
-; environment setup
-(define (setup-environment)
-  (let ((initial-env (extend-environment '() '() the-empty-environment)))
-    (extend-primitive-procedures
-     (extend-primitive-constructors
-      (extend-primitive-I/O-procedures initial-env)))))
+(setup-procedure '+ +)
+(setup-procedure '- -)
+(setup-procedure '* *)
+(setup-procedure '/ /)
+(setup-procedure '< <)
+(setup-procedure '> >)
+(setup-procedure '= =)
+(setup-procedure '<= <=)
+(setup-procedure '>= >=)
+
+; define/set!/and/or/list/quote/cond evaluation redirection
+(define (evaluate-special-form form environment)
+  (let ((name (car form)))
+    (case name
+      ((define) (evaluate-define form environment))
+      ((set!) (evaluate-set! form environment))
+      ((and) (evaluate-and form environment))
+      ((or) (evaluate-or form environment))
+      ((list) (evaluate-list form environment))
+      ((quote) (evaluate-list (append '(list) (cadr form)) environment))
+      ((values) (evaluate-values form environment))
+      ((cond) (evaluate-cond form environment)))))
+
+; define evaluation
+(define (evaluate-define expression environment)
+  (if (pair? (cadr expression))
+      (setup-procedure (caadr expression)
+                       (evaluate (cons 'lambda (cons (cdadr expression) (cons (cddr expression) '() ))) environment))
+      (setup-procedure (cadr expression)
+                       (evaluate (caddr expression) environment))))
+; set! evaluation
+(define (evaluate-set! expression environment)
+  (set-in-environment (cadr expression)
+                      (evaluate (caddr expression) environment)))
+
+; and evaluation
+(define (evaluate-and expression environment)
+  (if (null? (cdr expression))
+      #t
+      (and (evaluate (cadr expression) environment)
+           (evaluate-and (cdr expression) environment))))
+
+; or evaluation
+(define (evaluate-or expression environment)
+  (if (null? (cdr expression))
+      #f
+      (or (evaluate (cadr expression) environment)
+          (evaluate-or (cdr expression) environment))))
+
+; list-evaluation
+(define (evaluate-list expression environment)
+  (if (null? (cdr expression))
+      '()
+      (cons (evaluate (cadr expression) environment)
+            (evaluate-list (cdr expression) environment))))
+
+; values evaluation
+(define (evaluate-values expression environment)
+  (define result
+    (evaluate-list expression environment))
+  (apply values result))
+
+; cond evaluation
+(define  (evaluate-cond expression environment)
+  (let (
+        (case (caadr expression))
+        (action (cadadr expression)))
+    (cond ((eq? case 'else)
+           (evaluate action environment))
+          ((evaluate case environment)
+           (evaluate action environment))
+          (else (evaluate (cons (car expression) (cddr expression))
+                          environment)))))
+
+; helper function
+(define (zip list1 list2)
+  (cond ((null? list1) '())
+        ((null? list2) '())
+        (else (cons (cons (car list1) (car list2)) ( zip (cdr list1) (cdr list2))))))
--- scheme-interpreter.scm
+++ scheme-interpreter.scm
@@ -1,29 +1,71 @@
-#lang racket
+(#%require "scheme-interpreter-operations.scm")
+(#%provide (all-defined))
 ;; this is the function to start the interpreter
 
-(load "scheme-interpreter-operations.scm")
-(load "scheme-interpreter-types.scm")
-
 (define (interpret code)
-  (define (eval-exp exp env)
-    (cond ((self-evaluating? exp) exp)
-          ((variable? exp) (lookup-variable-value exp env))
-          ((quotation? exp) (text-of-quotation exp))
-          ((assignment? exp) (eval-assignment exp env))
-          ((definition? exp) (eval-definition exp env))
-          ((if? exp) (eval-if exp env))
-          ((lambda? exp)
-           (make-procedure (lambda-parameters exp)
-                           (lambda-body exp)
-                           env))
-          ((begin? exp) (eval-sequence (begin-actions exp) env))
-          ((cond? exp) (eval (cond-to-if exp) env))
-          ((application? exp)
-           (apply (eval (operator exp) env)
-                  (list-of-values (operands exp) env)))
-          (else (error "Unknown expression type -- EVAL" exp))))
-  (define global-env (setup-environment))
-  (eval-exp code global-env))
+  (evaluate (append '(values) code)
+            (lambda (object) (get-from-environment object))))
 
-(define code1 '(+ 1 2))
-
+(define evaluate
+  (lambda (expression environment)
+    (cond
+      ; number or bool -> return it
+      ((or (number? expression)
+           (boolean? expression))
+       expression)
+      ; (special-form-name . args) -> evaluate special form depending on its name
+      ((and
+        (pair? expression)
+        (special-form? (car expression)))
+       (evaluate-special-form expression environment))
+      ; ((not-lambda . ?) '(...))
+      ((and (pair? expression)
+            (pair? (car expression))
+            (not (eq? (caar expression) 'lambda))
+            (not(null? (cdr expression))))
+       (if (member (caar expression) '(define set!))
+           (begin
+             (evaluate (car expression) environment)
+             (evaluate (cdr expression) environment))
+           (
+            (evaluate (car expression) environment)
+            (evaluate (cadr expression) environment))))
+      ; (( . )) -> evaluate ( . )
+      ((and (pair? expression)
+            (pair? (car expression))
+            (null? (cdr expression)))
+       (evaluate (car expression) environment))
+      ; (f . args) -> evaluate args and then evaluate (f args)
+      ((and (pair? expression)
+            (symbol? (car expression))
+            (not (built-in-procedure? (car expression)))
+            (procedure? (environment (car expression))))
+       (apply (environment (car expression))
+              (map (lambda (expression)
+                     (evaluate expression environment))
+                   (cdr expression))))
+      ; '(...)
+      ((list? expression)
+       (case (car expression)
+         ; '(if p? t f) -> evaluate p? -> evaluate t or f based on the righteouness of p?
+         ((if)
+          (if (evaluate (cadr expression) environment)
+              (evaluate (caddr expression) environment)
+              (evaluate (cadddr expression) environment)))
+         ; '(map f l) -> evaluate f and apply a map to the evaluation of l 
+         ((map)
+          (map (evaluate (cadr expression) environment)
+               (evaluate (caddr expression) environment)))
+         ; '(lambda params body) -> lambda-env:= (zip params args); evaluate body in the environment 
+         ((lambda)
+          (lambda args
+            (let ((lambda-env (zip (cadr expression) args)))
+              (evaluate (caddr expression) (lambda (third)
+                                             (if (assoc third lambda-env)
+                                                 (cdr (assoc third lambda-env))
+                                                 (environment third)))))))
+         ; '(operator operand) -> evaluate each item
+         (else
+          ((evaluate (car expression) environment)
+           (evaluate (cadr expression) environment)))))
+      (else (environment expression)))))
